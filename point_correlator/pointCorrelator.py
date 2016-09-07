86a604bf541c389eeb71f6b64e4ef89fd7d42595
minimiz3d <arthur-357@hotmail.com>
2016-09-06 21:17:00 -0300

    Added pygame window setup

---
+++ point_correlator/pointCorrelator.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys, os
+import pygame
+from pygame.locals import *
+
+# Constants
+BACKGROUND_COLOR = (250,250,250)
+WIDTH, HEIGHT = 1024, 768
+# (...)
+
+class PointCorrelator:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT), RESIZABLE)
+
+    def draw(self):
+        self.screen.fill(BACKGROUND_COLOR)
+        pygame.display.flip()
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return False
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_ESCAPE:
+                    return False
+        return True
+
+    def mainLoop(self):
+        while self.events():
+            self.draw()
+
+
+# Exception Handling
+if __name__ == '__main__':
+    # (...)
+
+    pt_corr = PointCorrelator()
+    pt_corr.mainLoop()
