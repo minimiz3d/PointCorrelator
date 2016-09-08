4c1d78dfabe258a2aaa31a025b4b5281cd74c5d0
guichristmann <guichristmann@gmail.com>
2016-09-08 18:36:52 -0300

    Added highlighting to points

--- pointCorrelator/pointCorrelator.py
+++ pointCorrelator/pointCorrelator.py
@@ -2,6 +2,7 @@
 
 import sys, os
 import pygame
+import operator
 from operator import itemgetter
 from pygame.locals import *
 
@@ -9,7 +10,11 @@ from pygame.locals import *
 BACKGROUND_COLOR = (250,250,250)
 WIDTH, HEIGHT = 640, 480
 POINT_COLOR = (0, 150, 0)
+HIGHLIGHT_COLOR = (150, 0, 0)
 POINT_RADIUS = 3
+HIGHLIGHT_RADIUS = 5
+HIGHLIGHT_WIDTH = 2
+CLICK_PRECISION = 5
 
 class PointCorrelator(object):
     def __init__(self, pointsFile, imageFile):
@@ -17,6 +22,7 @@ class PointCorrelator(object):
         self.pointsFile = pointsFile
         self.imageFile = imageFile
         self.points_list = list()
+        self.highlighted_points = list()                                    # stores highlighted points
 
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT), RESIZABLE)   # set screen mode
         self.loadPoints()                                                   # loads file with point data
@@ -34,23 +40,51 @@ class PointCorrelator(object):
                                                                         # of the size max(x), max(y)
 
 #       self.ptsSurface = pygame.transform.scale(self.ptsSurface, (300, 300))       # resize the surface (only resizes the actual
-                                                                                    # surface, not what's drawn into it)
+                                                                                    # surface, not what's drawn in it)
+        
+        self.highlightSurface = pygame.Surface(self.ptsSurface.get_size())          # creates new surface with same size as ptsSurface
+                                                                                    # this will be used for highlighting points
+        self.highlightSurface.set_colorkey((0, 0, 0))                               # set transparency color to black
+
     def loadImage(self):
         self.imgSurface = pygame.image.load(self.imageFile)
 
-    def events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return False
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_ESCAPE: return False
-            elif event.type == VIDEORESIZE:
-                self.resize(event.w, event.h)
-        return True
-
     def resize(self, new_width, new_height):
-        self.screen = pygame.display.set_mode((new_width, new_height), RESIZABLE)   # set new mode for the screen when the window is resized
+        self.screen = pygame.display.set_mode((new_width, new_height), RESIZABLE)   # set new mode for the screen when the window 
+                                                                                    # is resized
 
+    # This method tests if a point clicked on the screen is in the points_list
+    # loaded from the file. This ensures that only points present in the list
+    # can be selected
+    def checkPoint(self, point_clicked):
+        print point_clicked
+        is_in = False
+        for point in self.points_list:
+            if point_clicked[0] < point[0] + CLICK_PRECISION and point_clicked[1] < point[1]\
+            + CLICK_PRECISION and point_clicked[0] > point[0] - CLICK_PRECISION \
+            and point_clicked[1] > point[1] - CLICK_PRECISION:                      # yeah, this is pretty fucking ugly
+                is_in = True
+                break
+        if is_in is True: return point        # returns point where the click happened
+        else: return None
+    
+    def highlightCircle(self, point_to_highlight):
+        if point_to_highlight is None:                          # checks if there's a point to highlight
+            return
+
+        already_highlighted = 0
+        for tup in self.highlighted_points:
+            if point_to_highlight == tup:
+                already_highlighted = 1
+                break
+
+        self.highlighted_points.append(point_to_highlight)      # adds point to the list of already highlighted points
+
+        if(already_highlighted == 0):
+            print "Highlighting %s" % (point_to_highlight, )
+            pygame.draw.circle(self.highlightSurface, HIGHLIGHT_COLOR, \
+                                point_to_highlight, HIGHLIGHT_RADIUS, HIGHLIGHT_WIDTH)
+    
     def draw(self):
         self.screen.fill(BACKGROUND_COLOR)
         for point in self.points_list:
@@ -59,12 +93,27 @@ class PointCorrelator(object):
         self.screen.blit(self.ptsSurface, (0, 0))               # blits ptsSurface
         offsetX = self.ptsSurface.get_rect()[2]                 # gets X offset for drawing the image
         self.screen.blit(self.imgSurface, (offsetX, 0))         # blits imgSurface to the right of ptsSurface
+        self.screen.blit(self.highlightSurface, (0,0))          # blits highlightSurface on the screen
         pygame.display.flip()                                   # actually shows shit on screen
-    
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return False
+            elif event.type == KEYUP:
+                if event.key == K_ESCAPE: return False
+            elif event.type == VIDEORESIZE:
+                self.resize(event.w, event.h)
+            elif event.type == MOUSEBUTTONUP:
+                self.highlightCircle(self.checkPoint(event.pos)) # this actually works
+
+        return True
+
+   
     def mainLoop(self):
         while self.events():
             self.draw()
-
+    
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         print "Usage: %s pointsFile.pts imageFile.img" % sys.argv[0]
