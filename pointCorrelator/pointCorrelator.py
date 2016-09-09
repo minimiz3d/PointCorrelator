1b19bf04ef8fd8905766b504e8a0ea1321790dd0
minimiz3d <arthur-357@hotmail.com>
2016-09-09 15:39:39 -0300

    Undo in progress

--- pointCorrelator/pointCorrelator.py
+++ pointCorrelator/pointCorrelator.py
@@ -127,32 +127,11 @@ class PointCorrelator(object):
             # Change circle's color
             self.highlightCircle(self.highlighted_points[-1], (0,0,0), 1)
             self.highlighted_points.pop()
+            self.connectedPoints.pop()
 
             print "Last highlighted_point removed.\n#highlighted_points: ", len(self.highlighted_points)
         else:
             print "No highlighted_points to remove."
-
-    # IN PROGRESS
-    # def drawHelp(self):
-    #     help_string = [
-    #         "ENTER - Save points coordinates into .txt file",
-    #         "LEFT_MOUSE_CLICK - Highlight point",
-    #         "CTRL + Z - Undo",
-    #         "ESC - Quit program"
-    #     ]
-    #
-    #     font = pygame.font.SysFont("monospace", 15)
-    #
-    #     for i, string in enumerate(help_string):
-    #         print_string = font.render(string, True, (255, 255, 255))
-    #         self.screen.blit()
-    #
-    #     for i in range(0, HEIGHT, 25):
-    #         surf = font.render(str(i), True, (0,0,0))
-    #         self.screen.blit(surf, (0, i - 10))
-    # # IN PROGRESS
-    # def blitSurface(self, surface, position):
-    #     self.screen['surface'].blit(surface, position)
 
     def events(self):
         for event in pygame.event.get():
